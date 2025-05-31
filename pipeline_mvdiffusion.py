abcf600a8bc5d850cb810a2f54a90f33ebfb2aca
Khoshafa <Abdualkader@users.noreply.huggingface.co>
2025-05-31 14:37:31 +0000

    Update pipeline_mvdiffusion.py

--- pipeline_mvdiffusion.py
+++ pipeline_mvdiffusion.py
@@ -65,7 +65,7 @@ class MVDiffusionPipeline(StableDiffusionPipeline):
             image_encoder=image_encoder,
             requires_safety_checker=requires_safety_checker,
         )
-        self.num_views = 32
+        self.num_views = 6
 
     def load_ip_adapter(
         self,
